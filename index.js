5a6d747524ed4ef5ff2b90093399c418d08fc747
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-05-24 00:01:55 +0300

    changes

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const path = require('path');
-const cors = require('cors');
+const express = require("express")
+const http = require("http")
+const socketIO = require("socket.io")
+const path = require("path")
+const cors = require("cors")
 
-const app = express();
-const httpServer = http.createServer(app);
+const app = express()
+const httpServer = http.createServer(app)
 
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use(express.static(path.join(__dirname, "public")))
 
 const io = socketIO(httpServer, {
     cors: {
-        origin: '*', // Allow all origins
-        methods: ['GET', 'POST'],
-        allowedHeaders: ['Content-Type'],
-        credentials: true
-    }
-});
+        origin: "*", // Allow all origins
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type"],
+        creudentials: true,
+    },
+})
 
-io.on('connection', (socket) => {
-    console.log('Client connected');
+io.on("connection", (socket) => {
+    console.log("Client connected")
 
-    socket.on('message1', (data) => {
-        console.log('Received from API ::', data);
-        io.emit('message2', data);
-    });
-});
+    socket.on("message1", (data) => {
+        console.log("Received from API ::", data)
+        io.emit("message2", data)
+    })
+})
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 httpServer.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    if (err) throw err
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
